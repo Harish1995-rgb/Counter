ea0d647c49c388d8c00e0e84d88be449acdafd5f
Harish H S <harish.hs@madrasglobal.com>
2021-02-17 16:06:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+var deadline = new Date("feb 21, 2020 15:37:25").getTime();
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var t = deadline - now;
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((t % (1000 * 60)) / 1000);
+    document.getElementById("Days").textContent = days;
+    document.getElementById("Hours").innerText = hours;
+    document.getElementById("Minutes").textContent = minutes;
+    document.getElementById("Seconds").textContent = seconds;
+    if (t < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+var colors = ['red', 'green', 'blue', 'orange', 'yellow', 'pink', 'cyan', '#006666', 'darkcyan', 'darkturquoise'];
+
+function add() {
+    var node = document.createElement('div');
+    node.classList.add('card');
+    node.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+    var s = prompt("Enter a wishes quotes");
+    if (s != null && s != '') {
+        node.innerHTML = '<i onclick="del()" class="far i fa-times-circle" style="float:right "></i><h3 id="wish">' + s + '</h3>';
+        document.getElementById("myDiv").appendChild(node);
+    }
+}
+
+function del() {
+    var list = document.getElementById("myDiv");
+    list.removeChild(list.firstChild);
+}
